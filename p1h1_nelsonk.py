48a8281538bac1a311d9d3c480ae03ed0b931b5f
nelsonk8078-coder <nelsonk8078@student.faytechcc.edu>
2026-09-08 18:01:27 +0000

    m1hw1 part 2

---
+++ p1h1_nelsonk.py
@@ -0,0 +1,14 @@
+#CTI-110
+#p1hw1- math
+#Nelson K
+#9/8/2026
+#dos some math processing
+
+start = int(input("enter the starting integer: "))
+#print("you typed", start)
+add_this = int(input("enter integer to add: "))
+sub_this = int(input("enter integer to subtract: "))
+answer = start + add_this -sub_this
+print("/n/n");
+print(start, "+", add_this, "-", sub_this, "is equal to", answer)
+print(f"{start} + {add_this} - {sub_this} is equal to {answer}")
