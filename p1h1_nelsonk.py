ef992c0453d33eeafa24c7d5cb3465ae0a5bbd80
nelsonk8078-coder <nelsonk8078@student.faytechcc.edu>
2026-09-08 18:10:53 +0000

    m1hw1 exp

--- p1h1_nelsonk.py
+++ p1h1_nelsonk.py
@@ -2,8 +2,14 @@
 #p1hw1- math
 #Nelson K
 #9/8/2026
-#dos some math processing
+#do some math processing
+print("-----Calculating Exponents-----")
+base = int(input("enter integer as base value: "))
+exponent =int(input("enter integer as exoponent: "))
+result = base ** exponent
+print(f"{base} to the {exponent} power is {result}")
 
+("-----Addition and Subtraction-----")
 start = int(input("enter the starting integer: "))
 #print("you typed", start)
 add_this = int(input("enter integer to add: "))
