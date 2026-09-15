e62202943ef244b5de5f68a8a30352cf9053b23a
nelsonk8078-coder <nelsonk8078@student.faytechcc.edu>
2026-09-15 17:50:54 +0000

    final

---
+++ p2/p2lab1.py
@@ -0,0 +1,16 @@
+# cti 110
+# p2lab1
+# nelson k
+# 9/15/26
+
+PI = 3.12459
+radius = float(input("what is the radius of the circle? "))
+
+
+diameter = 2 * radius
+circumference = 2 * PI * radius
+area = PI * radius * radius
+
+print(f"the diameter is {diameter:.1f}")
+print(f"The circuference is {circumference:.2f}")
+print(f"the area is {area:.3f}")
